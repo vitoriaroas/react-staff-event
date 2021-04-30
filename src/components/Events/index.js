9ed574e3330ec5c34b4d0a588cb943d0f64132df
nicole-allen679 <nicole.allen679@gmail.com>
2021-04-30 13:37:54 -0400

    Events

---
+++ src/components/Events/index.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+class Events extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            events: []
+        }
+    }
+    componentDidMount() {
+        fetch('https://events-84973.web.app/event')
+        .then(response => response.json())
+        .then(data => this.setState({events: data}))
+        .catch(err => console.log(err))
+    }
+    render() {
+        return (
+            <>
+            <h2>Events:</h2>
+            </>
+        )
+    }
+}
+
+export default Events
